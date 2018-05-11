d0d043ec8fba98fc0aba4167e08228bbe8ca8bde
KYJ <yzoo1008@snu.ac.kr>
2018-05-11 18:10:39 +0900

    modify train.py for run out of memory

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ test_y = np.zeros((len(test_ids), 32, 32, 1))
 cnt = 0
 for id in train_ids:
     img_path = "./data/resize/train/" + str(id) + ".jpg"
-    mask_path = "./data/mask/train/" + str(id) + ".npy"
+    mask_path = "./data/mask_npy/train/" + str(id) + ".npy"
     img = cv2.imread(img_path)
     mask = np.load(mask_path)
     train_x[cnt] = img
@@ -26,7 +26,7 @@ for id in train_ids:
 cnt = 0
 for id in test_ids:
     img_path = "./data/resize/test/" + str(id) + ".jpg"
-    mask_path = "./data/mask/test/" + str(id) + ".npy"
+    mask_path = "./data/mask_npy/test/" + str(id) + ".npy"
     img = cv2.imread(img_path)
     mask = np.load(mask_path)
     test_x[cnt] = img
@@ -37,6 +37,7 @@ for id in test_ids:
 learning_rate = 0.01
 num_epochs = 1000
 dropout_rate = 0.5
+batch_size = 32
 
 x = tf.placeholder(tf.float32, [None, 512, 512, 3])
 y = tf.placeholder(tf.float32, [None, 32, 32, 1])
@@ -66,11 +67,20 @@ prediction = tf.to_float(prediction_int)
 correct_prediction = tf.reduce_all(tf.equal(prediction, y))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
+# train_x_batch, train_y_batch = tf.train.batch([train_x, train_y], batch_size=batch_size)
+# x_batch, y_batch = sess.run([train_x_batch, train_y_batch])
 
-    for epoch in range(num_epochs):
-        sess.run(apply_gradients, feed_dict={x: train_x, y: train_y, keep_prob: dropout_rate})
-        if epoch%10==0:
-			loss, acc = sess.run([cost, accuracy], feed_dict={x: train_x, y: train_y, keep_prob: dropout_rate})
-			print("Step: {:5}\tLoss: {:.3f}\tAcc:{:.2%}".format(epoch, loss, acc))
+with tf.Session() as sess:
+	sess.run(tf.global_variables_initializer())
+	for epoch in range(num_epochs):
+		avg_loss = 0
+		total_batch = int(cnt/batch_size)
+		for i in range(total_batch):
+			x_batch, y_batch = train_x[i*batch_size:(i+1)*batch_size], train_y[i*batch_size:(i+1)*batch_size]
+			feed_dict = {x: x_batch, y: y_batch, keep_prob: dropout_rate}
+			loss, acc, _, s = sess.run([cost, accuracy, apply_gradients, score], feed_dict=feed_dict)
+			avg_loss += loss / total_batch
+			for r in range(32):
+				for c in range(32):
+					print(s.shape)
+		print("Step: {:5}\tLoss: {:.3f}\tAcc:{:.2%}".format(epoch, avg_loss, acc))
