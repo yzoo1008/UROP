9a0406af6767eec3d73e96588c596de0b00d9382
KYJ <yzoo1008@snu.ac.kr>
2018-05-14 17:25:54 +0900

    apply AdamOptimizer

--- train.py
+++ train.py
@@ -33,9 +33,6 @@ for id in test_ids:
     test_y[cnt_test] = mask
     cnt_test += 1
 
-
-starter_learning_rate = 0.1
-learning_decay = 0.96
 NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN = cnt_train
 NUM_EXAMPLES_PER_EPOCH_FOR_TEST = cnt_test
 NUM_EPOCHES_PER_DECAY = 350.0
@@ -53,35 +50,41 @@ keep_prob = tf.placeholder(tf.float32)
 model = AlexNet(x, keep_prob)
 score = model.conv6
 
+learning_rate = INITIAL_LEARNING_RATE
 ms = tf.reduce_mean(tf.square(score-y))
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(ms)
 
-global_step = tf.train.get_or_create_global_step()
+global_step = tf.Variable(0, trainable=False)
 num_batches_per_epoch = int(NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN / batch_size)
 decay_steps = int(num_batches_per_epoch * NUM_EPOCHES_PER_DECAY)
+'''
 learning_rate = tf.train.exponential_decay(
-		starter_learning_rate,
+		INITIAL_LEARNING_RATE,
 		global_step,
 		decay_steps,
 		LEARNING_RATE_DECAY_FACTOR,
-		staircase=True
-		)
+		staircase=True)
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate)
 gvs = optimizer.compute_gradients(ms)
 apply_gradient_op = optimizer.apply_gradients(gvs, global_step=global_step)
+'''
+
 
 '''
 prediction_int = tf.to_int32(score > 0.6)
 prediction = tf.to_float(prediction_int)
 correct_prediction = tf.reduce_all(tf.equal(prediction, y))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+train_x_batch, train_y_batch = tf.train.batch([train_x, train_y], batch_size=batch_size)
+x_batch, y_batch = sess.run([train_x_batch, train_y_batch])
 '''
 
-# train_x_batch, train_y_batch = tf.train.batch([train_x, train_y], batch_size=batch_size)
-# x_batch, y_batch = sess.run([train_x_batch, train_y_batch])
-
 with tf.Session() as sess:
+
 	sess.run(tf.global_variables_initializer())
+	
 	for epoch in range(num_epochs):
 		avg_loss = 0.0
 		num_truth = 0
@@ -92,22 +95,27 @@ with tf.Session() as sess:
 		for i in range(tb):
 			x_batch, y_batch = train_x[i*batch_size:(i+1)*batch_size], train_y[i*batch_size:(i+1)*batch_size]
 			feed_dict = {x: x_batch, y: y_batch, keep_prob: dropout_rate}
-			loss, _, sc = sess.run([ms, apply_gradient_op, score], feed_dict=feed_dict)
+#			loss, _, sc, lr = sess.run([ms, apply_gradient_op, score, learning_rate], feed_dict=feed_dict)
+			loss, _, sc = sess.run([ms, optimizer, score], feed_dict=feed_dict)
 			avg_loss += loss / tb
 			
 			for index in range(batch_size):
-				for i in range(32):
-					for j in range(32):
-						if sc[index][i][j] > 150.0:
+				for row in range(32):
+					for col in range(32):
+						if sc[index][row][col] > 150.0:
 							num_predict_truth += 1
-							if y_batch[index][i][j] == 255.0:
+							if y_batch[index][row][col] == 255.0:
 								correct_answer += 1
-						if y_batch[index][i][j] == 255.0:
+						if y_batch[index][row][col] == 255.0:
 							num_truth += 1
+						
+#						if sc[index][row][col] != 0.0:
+#							print(sc[index][row][col], index, row, col)
+
+			print("Step: {:5d}\t Num_Batch: {:5d}\tLoss: {:.3f}\t".format(epoch, i, loss))
 #						if y_batch[index][i][j] == 255.0:
 #							print(y_batch[index][i][j], index, i, j)
-#						if sc[index][i][j] != 0.0:
-#							print(sc[index][i][j], index, i, j)
+
 		if num_predict_truth == 0:
 			print("Step: {:5d}\tLoss: {:.3f}\tRecall: {:.3f}".format(epoch, avg_loss, correct_answer/num_truth))
 			print("num_predict_truth: {:5d}".format(num_predict_truth))
