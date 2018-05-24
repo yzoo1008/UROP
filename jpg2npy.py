157734804c3ec15fb77ca5162e5e023aecd72d7d
KYJ <yzoo1008@snu.ac.kr>
2018-05-24 13:46:25 +0900

    jpg2npy

---
+++ jpg2npy.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+
+test_ids = open("./data/test/ids.txt").read().splitlines()
+train_ids = open("./data/train/ids.txt").read().splitlines()
+whole_ids = {"test": test_ids, "train": train_ids}
+
+for case in whole_ids:
+	for id in whole_ids[case]:
+		img_path = "./data/resize/" + case + "/" + str(id) + ".jpg"
+		img = cv2.imread(img_path)
+		print(img.shpae())
+		np.save("./data/resize_npy/" + case + "/" + str(id), img)
