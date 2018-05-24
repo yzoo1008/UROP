471d49af5003dcc40ba6f18eb730bf165d131194
KYJ <yzoo1008@snu.ac.kr>
2018-05-24 15:23:38 +0900

    update alexnet.py

--- alexnet.py
+++ alexnet.py
@@ -1,100 +1,148 @@
 import tensorflow as tf
+import numpy as np
 
 
 class AlexNet(object):
 
-    def __init__(self, x, keep_prob):
-        self.X = x
-        self.KEEP_PROB = keep_prob
-        self.create()
+	def __init__(self, x, keep_prob, num_classes, skip_layer, weights_path='NONE'):
 
-    def create(self):
-        # 1st Layer: Conv (w ReLu) -> Lrn -> Pool
-        conv1 = conv(self.X, 11, 11, 96, 4, 4, name='conv1')                    # 512x512x3 => 128x128x96
-        norm1 = lrn(conv1, 2, 1e-05, 0.75, name='norm1')
-        pool1 = max_pool(norm1, 3, 3, 2, 2, name='pool1')                       # 128x128x96 => 64x64x96
+		self.X = x
+		self.NUM_CLASSES = num_classes
+		self.KEEP_PROB = keep_prob
+		self.SKIP_LAYER = skip_layer
 
-        # 2nd Layer: Conv (w ReLu)  -> Lrn -> Pool with 2 groups
-        conv2 = conv(pool1, 5, 5, 256, 1, 1, groups=2, name='conv2')            # 64x64x96 => 64x64x256
-        norm2 = lrn(conv2, 2, 1e-05, 0.75, name='norm2')
-        pool2 = max_pool(norm2, 3, 3, 2, 2, name='pool2')                       # 64x64x256 => 32x32x256
+		if weights_path == 'NONE':
+			self.WEIGHTS_PATH = None
+		elif weights_path == 'DEFAULT':
+			self.WEIGHTS_PATH = 'bvlc_alexnet.npy'
+		else:
+			self.WEIGHTS_PATH = weights_path
 
-        # 3rd Layer: Conv (w ReLu)
-        conv3 = conv(pool2, 3, 3, 384, 1, 1, name='conv3')                      # 32x32x256 => 32x32x384
+		self.create()
 
-        # 4th Layer: Conv (w ReLu) splitted into two groups
-        conv4 = conv(conv3, 3, 3, 384, 1, 1, groups=2, name='conv4')            # 32x32x384 => 32x32x384
+	def create(self):
 
-        # 5th Layer: Conv (w ReLu) -> Pool splitted into two groups
-        conv5 = conv(conv4, 3, 3, 256, 1, 1, groups=2, name='conv5')            # 32x32x384 => 32x32x256
+		# 1st Layer: Conv (w ReLu) -> Pool -> Lrn
+		conv1 = conv(self.X, 11, 11, 96, 4, 4, name='conv1')            # 512x512x3 -> 128x128x96
+		pool1 = max_pool(conv1, 3, 3, 2, 2, name='pool1')               # 128x128x96 -> 64x64x96
+		norm1 = lrn(pool1, 2, 2e-05, 0.75, name='norm1')
 
-        self.conv6 = conv(conv5, 3, 3, 1, 1, 1, name='conv6')                        # 32x32x256 => 32x32x1
+		# 2nd Layer: Conv (w ReLu) -> Pool -> Lrn with 2 groups
+		conv2 = conv(norm1, 5, 5, 256, 1, 1, groups=2, name='conv2')    # 64x64x96 -> 64x64x256
+		pool2 = max_pool(conv2, 3, 3, 2, 2, name='pool2')               # 64x64x256 -> 32x32x256
+		norm2 = lrn(pool2, 2, 2e-05, 0.75, name='norm2')
 
-#        flattened = tf.reshape(conv6, [-1, 32 * 32 * 1])
-#        fc7 = fc(flattened, 32 * 32 * 1, 1024, name='fc6')
-#        dropout7 = dropout(fc7, self.KEEP_PROB)
+		# 3rd Layer: Conv (w ReLu)
+		conv3 = conv(norm2, 3, 3, 384, 1, 1, name='conv3')              # 32x32x256 -> 32x32x384
 
-        # 7th Layer: FC (w ReLu) -> Dropout
-#        self.fc8 = fc(dropout7, 1024, 1024, name='fc7')
+		# 4th Layer: Conv (w ReLu) splitted into two groups
+		conv4 = conv(conv3, 3, 3, 384, 1, 1, groups=2, name='conv4')    # 32x32x384 -> 32x32x384
+
+		# 5th Layer: Conv (w ReLu) -> Pool splitted into two groups
+		conv5 = conv(conv4, 3, 3, 256, 1, 1, groups=2, name='conv5')    # 32x32x256 -> 32x32x256
+
+		# 6th Layer: Conv (w ReLu)
+		conv6 = conv(conv5, 3, 3, 64, 1, 1, name='conv6')               # 32x32x256 -> 32x32x64
+
+		# 7th Layer: Conv (w ReLu)
+		conv7 = conv(conv6, 3, 3, 16, 1, 1, name='conv6')               # 32x32x64 -> 32x32x16
+
+		# 8th Layer: Conv (w ReLu)
+		self.conv8 = conv(conv7, 3, 3, 1, 1, 1, name='conv6')           # 32x32x16 -> 32x32x1
+
+
+	def load_initial_weights(self, session):
+
+		# Load the weights into memory
+		weights_dict = np.load(self.WEIGHTS_PATH, encoding='bytes').item()
+
+		# Loop over all layer names stored in the weights dict
+		for op_name in weights_dict:
+
+			# Check if the layer is one of the layers that should be reinitialized
+			if op_name not in self.SKIP_LAYER:
+
+				with tf.variable_scope(op_name, reuse=True):
+
+					# Loop over list of weights/biases and assign them to their corresponding tf variable
+					for data in weights_dict[op_name]:
+
+						# Biases
+						if len(data.shape) == 1:
+
+							var = tf.get_variable('biases', trainable=False)
+							session.run(var.assign(data))
+
+						# Weights
+						else:
+
+							var = tf.get_variable('weights', trainable=False)
+							session.run(var.assign(data))
+
+
 
 def conv(x, filter_height, filter_width, num_filters, stride_y, stride_x, name, padding='SAME', groups=1):
-    # Get number of input channels
-    input_channels = int(x.get_shape()[-1])
 
-    # Create lambda function for the convolution
-    convolve = lambda i, k: tf.nn.conv2d(i, k,
-                                         strides=[1, stride_y, stride_x, 1],
-                                         padding=padding)
+	# Get number of input channels
+	input_channels = int(x.get_shape()[-1])
 
-    with tf.variable_scope(name) as scope:
-        weights = tf.get_variable('weights', shape=[filter_height,
-                                                    filter_width,
-                                                    input_channels / groups,
-                                                    num_filters])
-        biases = tf.get_variable('biases', shape=[num_filters])
+	# Create lambda function for the convolution
+	convolve = lambda i, k: tf.nn.conv2d(i, k, strides=[1, stride_y, stride_x, 1], padding=padding)
 
-    if groups == 1:
-        conv = convolve(x, weights)
+	with tf.variable_scope(name) as scope:
+		# Create tf variables for the weights and biases of the conv layer
+		weights = tf.get_variable('weights', shape=[filter_height, filter_width, input_channels / groups, num_filters])
+		biases = tf.get_variable('biases', shape=[num_filters])
 
-    # In the cases of multiple groups, split inputs & weights and
-    else:
-        # Split input and weights and convolve them separately
-        input_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
-        weight_groups = tf.split(axis=3, num_or_size_splits=groups,
-                                 value=weights)
-        output_groups = [convolve(i, k) for i, k in zip(input_groups, weight_groups)]
+		if groups == 1:
+			conv = convolve(x, weights)
 
-        # Concat the convolved output together again
-        conv = tf.concat(axis=3, values=output_groups)
+		# In the cases of multiple groups, split inputs & weights and
+		else:
+			# Split input and weights and convolve them separately
+			input_groups = tf.split(axis=3, num_or_size_splits=groups, value=x)
+			weight_groups = tf.split(axis=3, num_or_size_splits=groups, value=weights)
+			output_groups = [convolve(i, k) for i, k in zip(input_groups, weight_groups)]
 
-    bias = tf.reshape(tf.nn.bias_add(conv, biases), tf.shape(conv))
+			# Concat the convolved output together again
+			conv = tf.concat(axis=3, values=output_groups)
 
-    relu = tf.nn.relu(bias, name=scope.name)
+		# Add biases
+		bias = tf.reshape(tf.nn.bias_add(conv, biases), conv.get_shape().as_list())
 
-    return relu
+		# Apply relu function
+		relu = tf.nn.relu(bias, name=scope.name)
+
+		return relu
+
 
 def fc(x, num_in, num_out, name, relu=True):
-    with tf.variable_scope(name) as scope:
-        weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
-        biases = tf.get_variable('biases', [num_out], trainable=True)
+	with tf.variable_scope(name) as scope:
 
-        act = tf.nn.xw_plus_b(x, weights, biases, name=scope.name)
+		# Create tf variables for the weights and biases
+		weights = tf.get_variable('weights', shape=[num_in, num_out], trainable=True)
+		biases = tf.get_variable('biases', [num_out], trainable=True)
 
-    if relu:
-        relu = tf.nn.relu(act)
-        return relu
-    else:
-        return act
+		# Matrix multiply weights and inputs and add bias
+		act = tf.nn.xw_plus_b(x, weights, biases, name=scope.name)
+
+		if relu == True:
+			# Apply ReLu non linearity
+			relu = tf.nn.relu(act)
+			return relu
+		else:
+			return act
+
 
 def max_pool(x, filter_height, filter_width, stride_y, stride_x, name, padding='SAME'):
-    return tf.nn.max_pool(x, ksize=[1, filter_height, filter_width, 1],
-                          strides=[1, stride_y, stride_x, 1],
-                          padding=padding, name=name)
+	return tf.nn.max_pool(x, ksize=[1, filter_height, filter_width, 1],
+						  strides=[1, stride_y, stride_x, 1],
+						  padding=padding, name=name)
+
 
 def lrn(x, radius, alpha, beta, name, bias=1.0):
-    return tf.nn.local_response_normalization(x, depth_radius=radius,
-                                              alpha=alpha, beta=beta,
-                                              bias=bias, name=name)
+	return tf.nn.local_response_normalization(x, depth_radius=radius, alpha=alpha, beta=beta, bias=bias, name=name)
+
 
 def dropout(x, keep_prob):
-    return tf.nn.dropout(x, keep_prob)
+	return tf.nn.dropout(x, keep_prob)
