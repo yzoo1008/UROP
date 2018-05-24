311a19bf4e60c2cd104890f0a20ce1ed53a234b0
KYJ <yzoo1008@snu.ac.kr>
2018-05-24 21:07:40 +0900

    add dataset.py

--- dataset.py
+++ dataset.py
@@ -4,13 +4,16 @@ import cv2
 
 class DataSet:
 
-	def __init__(self, mode='train'):
+	def __init__(self, mode='train', in_size = (512, 512), out_size = (32, 32)):
 
 		self.x = []
 		self.y = []
 		self.pointer = 0
 		self.mode = mode
+		self.in_size = in_size
+		self.out_size = out_size
 		self.ids = open('./data/' + mode + '/ids.txt').read().splitlines()
+		self.data_size = (len(self.ids))
 
 		if mode == 'train':
 			self.ids = "./data/train/ids.txt"
@@ -24,44 +27,31 @@ class DataSet:
 		for id in self.ids:
 			img_path = './data/resize/' + str(self.mode) + '/' + str(id) + '.jpg'
 			mask_path = './data/mask_npy/' + str(self.mode) + '/' + str(id) + ".npy"
-			img = cv2.imread(img_path)
-			mask = np.load(mask_path)
-			self.x.append(img)
-			self.y.append(mask)
-			self.data_size(len(img))
+			self.x.append(img_path)
+			self.y.append(mask_path)
 
 
 	def next_batch(self, batch_size):
 
 		# Get next batch of image (path) and labels
-		paths = self.images[self.pointer:self.pointer + batch_size]
-		labels = self.labels[self.pointer:self.pointer + batch_size]
+		imgs_path = self.x[self.pointer:self.pointer + batch_size]
+		masks_path = self.y[self.pointer:self.pointer + batch_size]
 
 		#update pointer
 		self.pointer += batch_size
 
 		# Read images
-		images = np.ndarray([batch_size, self.scale_size[0], self.scale_size[1], 3])
-		for i in range(len(paths)):
-			img = cv2.imread(paths[i])
-
-			#flip image at random if flag is selected
-			if self.horizontal_flip and np.random.random() < 0.5:
-				img = cv2.flip(img, 1)
-
-			#rescale image
-			img = cv2.resize(img, (self.scale_size[0], self.scale_size[1]))
+		images = np.ndarray([batch_size, self.in_size[0], self.in_size[1], 3])
+		for i in range(len(imgs_path)):
+			img = cv2.imread(imgs_path[i])
 			img = img.astype(np.float32)
-
-			#subtract mean
-			img -= self.mean
-
 			images[i] = img
 
-		# Expand labels to one hot encoding
-		one_hot_labels = np.zeros((batch_size, self.n_classes))
-		for i in range(len(labels)):
-			one_hot_labels[i][labels[i]] = 1
+		masks = np.ndarray([batch_size, self.out_size[0], self.out_size[1], 1])
+		for i in range(len(masks_path)):
+			mask = np.load(masks_path[i])
+			mask = mask.astype(np.float32)
+			masks[i] = mask
 
 		#return array of images and labels
-		return images, one_hot_labels
+		return images, masks
