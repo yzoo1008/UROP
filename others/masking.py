95617fc5ede114b236d82e9fd69f0af496251d50
KYJ <yzoo1008@snu.ac.kr>
2018-05-28 14:06:39 +0900

    modify mask.npy files
    0, 255 -> -1, 1 for tanh

--- others/masking.py
+++ others/masking.py
@@ -28,32 +28,15 @@ for case in whole_ids:
 			x_max = int(math.floor(float(x_max)*X_out/X))
 			y_max = int(math.floor(float(y_max)*Y_out/Y))
 
-			for y in range(y_min, y_max+1):
-				for x in range(x_min, x_max+1):
-					mask[y, x] = 255.0
-#cv2.imwrite("./data/mask/" + case + "/" + str(id) + ".jpg", mask)
+			for row in range(Y_out):
+				for col in range(X_out):
+					mask[row, col] = -1.
+					if y_max >= row >= y_min:
+						if x_max >= col >= x_min:
+							mask[row, col] = 1.
+
+#			for y in range(y_min, y_max+1):
+#				for x in range(x_min, x_max+1):
+#					mask[y, x] = 1
 		np.save("./data/mask_npy/" + case + "/" + str(id), mask)
 
-
-'''
-arr = np.load("./data/mask_npy/train/9998.npy")
-print(np.shape(arr))
-for i in range(X_out):
-	for j in range(Y_out):
-		print(arr[i][j])
-'''
-'''
-img_d = cv2.imread("./data/mask/train/9998.jpg")
-img = cv2.imread("./data/mask/train/9998.jpg", cv2.IMREAD_GRAYSCALE)
-img_reshape = np.reshape(img, (32, 32, 1))
-print(np.shape(img_d))
-print(np.shape(img))
-print(np.shape(img_reshape))
-for i in range(X_out):
-	for j in range(Y_out):
-		print(img[i][j], img_reshape[i][j][0])
-print("------------------------------------")
-for i in range(X_out):
-	for j in range(Y_out):
-		print(img_d[i][j])
-'''
