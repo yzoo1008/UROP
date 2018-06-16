590e1e00d5f9739e5200cf25584eee03108a1b6a
KYJ <yzoo1008@snu.ac.kr>
2018-06-16 17:44:35 +0900

    update get_threshold

--- dataset.py
+++ dataset.py
@@ -64,5 +64,6 @@ class DataSet:
 
 
 	def get_threshold(self):
-		return np.reshape(self.threshold, [-1, self.out_size, self.out_size, 1])
+		return self.threshold
+#		return np.reshape(self.threshold, [-1, self.out_size, self.out_size, 1])
 
