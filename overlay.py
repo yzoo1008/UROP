08c7d9f164683c3bba41d4fc50aa0cb4fe1a3c5c
KYJ <yzoo1008@snu.ac.kr>
2018-06-21 13:57:17 +0900

    update overlay.py

--- overlay.py
+++ overlay.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-
+import os
 
 def npy2img(npy):
 	img = np.zeros((32, 32, 3))
@@ -13,21 +13,20 @@ def npy2img(npy):
 
 
 ids = open('./test_ids.txt').read().splitlines()
-alpha = 0.4
+alpha = 0.5
 
 for id in ids:
 	image = cv2.imread('./data/resize/test/' + str(id) + '.jpg')
 	threshold_npy = np.load('./data/threshold_npy/test/' + str(id) + '.npy')
 	threshold = npy2img(threshold_npy)
 	threshold_resize = cv2.resize(threshold, (512, 512))
+	cv2.imwrite('./test.jpg', threshold_resize)
+	overlay = cv2.imread('./test.jpg')
+	output = image.copy()
 
-	overlay = threshold_resize.copy()
-	output = threshold_resize.copy()
-
-	cv2.rectangle(overlay, (420, 205), (595, 385), (0, 0, 255), -1)
-	cv2.putText(overlay, "alpha={}".format(alpha), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 0, 255), 3)
 	cv2.addWeighted(overlay, alpha, output, 1 - alpha, 0, output)
 
 	cv2.imshow("Output", output)
 	cv2.waitKey(0)
 
+os.remove('./test.jpg')
